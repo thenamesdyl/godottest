5c57fb9bd63d92763ec2881fe306b3fdfea72adc
thenamesdyl <burtndylan@gmail.com>
2025-03-30 17:45:39 -0700

    update

---
+++ scripts/global/globals.gd
@@ -0,0 +1,56 @@
+extends Node
+
+## Constants
+const SAVE_FILE_PATH = "user://save_data.json"
+const MAX_PLAYERS = 4
+const DEBUG_MODE = true
+
+## Enums
+enum GameState {
+	MENU,
+	PLAYING,
+	PAUSED,
+	GAME_OVER
+}
+
+## Variables
+var current_game_state: int = GameState.MENU
+var player_score: int = 0
+var high_score: int = 0
+
+## Signals
+signal game_state_changed(new_state)
+signal score_updated(new_score)
+
+## Lifecycle Methods
+func _ready() -> void:
+	load_high_score()
+
+## Public Methods
+func change_game_state(new_state: int) -> void:
+	current_game_state = new_state
+	game_state_changed.emit(new_state)
+
+func update_score(points: int) -> void:
+	player_score += points
+	score_updated.emit(player_score)
+	
+	if player_score > high_score:
+		high_score = player_score
+		save_high_score()
+
+## Private Methods
+func load_high_score() -> void:
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	if file:
+		var data = JSON.parse_string(file.get_as_text())
+		if data and data.has("high_score"):
+			high_score = data.high_score
+		file.close()
+
+func save_high_score() -> void:
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	if file:
+		var data = {"high_score": high_score}
+		file.store_string(JSON.stringify(data))
+		file.close() 
---
+++ scripts/main_screen.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+func _ready():
+	# Initial setup when the scene loads
+	pass
+
+func _process(_delta):
+	# Process function for any future updates we might need
+	pass 
